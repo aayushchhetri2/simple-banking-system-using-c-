ef307b38381bbfff9a6d8ed5664ac003ada73aba
Aayush Lamichhae <83197433+aayushchhetri2@users.noreply.github.com>
2023-07-21 21:16:47 +0545

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,37 @@
+﻿public class Account
+{
+    public int AccountNumber { get; private set; }
+    public string AccountHolderName { get; private set; }
+    public decimal Balance { get; private set; }
+
+    public Account(int accountNumber, string accountHolderName)
+    {
+        AccountNumber = accountNumber;
+        AccountHolderName = accountHolderName;
+        Balance = 0;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        Balance += amount;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount > Balance)
+        {
+            Console.WriteLine("Insufficient funds.");
+        }
+        else
+        {
+            Balance -= amount;
+        }
+    }
+
+    public void DisplayBalance()
+    {
+        Console.WriteLine($"Account Holder: {AccountHolderName}");
+        Console.WriteLine($"Account Number: {AccountNumber}");
+        Console.WriteLine($"Balance: {Balance:C}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Security.Principal;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Simple Banking System!");
+
+        // Create a new account
+        Console.Write("Enter your account number: ");
+        int accountNumber = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter your name: ");
+        string accountHolderName = Console.ReadLine();
+
+        Account account = new Account(accountNumber, accountHolderName);
+
+        // Perform transactions
+        while (true)
+        {
+            Console.WriteLine("\nSelect an option:");
+            Console.WriteLine("1. Deposit");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Check Balance");
+            Console.WriteLine("4. Exit");
+
+            int option = int.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.Write("Enter the amount to deposit: ");
+                    decimal depositAmount = decimal.Parse(Console.ReadLine());
+                    account.Deposit(depositAmount);
+                    break;
+
+                case 2:
+                    Console.Write("Enter the amount to withdraw: ");
+                    decimal withdrawAmount = decimal.Parse(Console.ReadLine());
+                    account.Withdraw(withdrawAmount);
+                    break;
+
+                case 3:
+                    account.DisplayBalance();
+                    break;
+
+                case 4:
+                    Console.WriteLine("Thank you for using Simple Banking System. Goodbye!");
+                    return;
+
+                default:
+                    Console.WriteLine("Invalid option. Please try again.");
+                    break;
+            }
+        }
+    }
+}
